6e2b93951b91c9af42ec47373e8b429f36a7a984
@cookie <111171388+rahatmughal@users.noreply.github.com>
2024-09-02 16:43:23 +0500

    :tada: Application Launched!

---
+++ DataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert("Hello World!");
+
+console.log(3 + 3);
+console.log("Rahat");
+
+
+let name = "rahat"
+let age = 18
+let isLoggedIn = false;
+
+// number =>2 to power 53
+//  bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol=> unique
+
+
+
+// objects
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
---
+++ variable.js
@@ -0,0 +1,18 @@
+const accountId = 1232332;
+let accountEmail = "rahatalibaig810@gmail.com";
+var accountPassword = "12345";
+accountCity = "Rawalpindi";
+let  accountState;
+// accountId = 2;
+
+accountEmail = "rahatalibaig@gmail.com";
+accountPassword = "212131";
+accountCity = "Islamabad";
+
+/*
+var is used for global scope and let is used for local scope of functional scope
+* */
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
